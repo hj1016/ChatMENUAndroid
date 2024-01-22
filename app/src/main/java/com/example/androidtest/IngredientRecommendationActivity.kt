fb639bacfa240671e3db258e5bab791d1dd9e372
catapillar0505 <twrp3301@gmail.com>
2024-01-22 11:09:41 +0900

    냉장고 속 재료로 추천받기 _ class와 xml 생성

---
+++ app/src/main/java/com/example/androidtest/IngredientRecommendationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidtest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class IngredientRecommendationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingredient_recommendation)
+    }
+}

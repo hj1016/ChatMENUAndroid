e32903f8ff5eb156f18fe6f284bd0876dca7cbc4
jiwon <knowledgeis1ne@gmail.com>
2024-01-23 22:18:54 +0900

    채팅으로 추천받기 화면 구성

---
+++ app/src/main/java/com/example/androidtest/ChatRecommendationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatRecommendationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_recommendation)
+
+    }
+}

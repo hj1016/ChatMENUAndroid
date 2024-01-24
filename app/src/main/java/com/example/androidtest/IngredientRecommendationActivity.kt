d8b15e25ece19609dbd901641293a7b5dced94ab
catapillar0505 <twrp3301@gmail.com>
2024-01-25 04:00:44 +0900

    1. 냉장고 속 재료로 추천받기 _ 리스트뷰 추가 완료 2. 메인화면 대강 구성

--- app/src/main/java/com/example/androidtest/IngredientRecommendationActivity.kt
+++ app/src/main/java/com/example/androidtest/IngredientRecommendationActivity.kt
@@ -1,11 +1,100 @@
 package com.example.androidtest
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import java.util.zip.Inflater
 
 class IngredientRecommendationActivity : AppCompatActivity() {
+    // UI 요소 선언
+    private lateinit var button_ir_plus : Button
+    private lateinit var button_ir_complete : Button
+    private lateinit var editText_ir_name : EditText
+    private lateinit var button_ir_confirm : Button
+    private lateinit var button_ir_cancel : Button
+    private lateinit var listview_ir: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingredient_recommendation)
+
+        // UI 요소 초기화
+        button_ir_plus = findViewById(R.id.button_ir_plus)
+        button_ir_complete = findViewById(R.id.button_ir_complete)
+
+
+        //리스트뷰와 어댑터 초기화
+        listview_ir = findViewById<ListView>(R.id.listview_ir)
+        val ingredientsList = ArrayList<String>()
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, ingredientsList)
+        listview_ir.adapter = adapter
+
+        // 추가 버튼 터치시 동작
+        try{
+            button_ir_plus.setOnClickListener {
+                // 재료 이름을 입력하는 레이어 팝업
+                enterIngredientNamePopup()
+            }
+        }catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+
     }
-}
+    // 재료 이름을 입력하는 레이어 팝업
+    private fun enterIngredientNamePopup() {
+
+        //레이어 팝업 UI 초기화
+        val builder = AlertDialog.Builder(this)
+        val inflater = LayoutInflater.from(this)
+        val view = inflater.inflate(R.layout.popup_ingredient_name, null)
+
+        editText_ir_name = view.findViewById<EditText>(R.id.editText_ir_name)
+        button_ir_confirm =view.findViewById<Button>(R.id.button_ir_confirm)
+        button_ir_cancel = view.findViewById<Button>(R.id.button_ir_cancel)
+
+        // 팝업 생성
+        builder.setView(view)
+        val dialog = builder.create()
+
+        button_ir_confirm.setOnClickListener {
+            val ingredientName = editText_ir_name.text.toString()
+            // 이름을 사용하여 버튼 생성
+            addIngredient(ingredientName)
+            dialog.dismiss() // 팝업 닫기
+        }
+
+        button_ir_cancel.setOnClickListener {
+           dialog.dismiss() // 팝업 닫기
+       }
+        // 팝업 띄우기
+        dialog.show()
+    }
+
+    //재료 리스트뷰에 추가
+    private fun addIngredient(ingredientName: String) {
+        // 입력 재료 이름 가져오기
+        try{
+            val ingredientName = editText_ir_name.text.toString()
+            if (ingredientName.isNotEmpty()) {
+                // 리스트에 항목 추가
+                adapter.add(ingredientName)
+                // 입력창 초기화
+                editText_ir_name.text.clear()
+            }
+        }catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    }
+
--- app/src/main/java/com/example/androidtest/MainActivity.kt
+++ app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.androidtest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    //ui 요소 선언
+    lateinit var button_main_chat : Button
+    lateinit var button_main_ir : Button
+    lateinit var button_main_time : Button
+    lateinit var button_main_weather : Button
+    lateinit var button_main_feeling : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ui 요소 초기화
+        button_main_ir = findViewById(R.id.button_main_ir) //IngredientRecommendationActiviy로 이동하는 버튼
+        button_main_chat = findViewById(R.id.button_main_chat)
+        button_main_time = findViewById(R.id.button_main_time)
+        button_main_weather = findViewById(R.id.button_main_weather)
+        button_main_feeling = findViewById(R.id.button_main_feeling)
+
+        //화면 전환
+        button_main_ir.setOnClickListener {
+            val intent = Intent(this, IngredientRecommendationActivity::class.java)
+            startActivity(intent)
+        }
+        button_main_chat.setOnClickListener {
+            val intent = Intent(this, ChatRecommendationActivity::class.java)
+            startActivity(intent)
+        }
+        button_main_time.setOnClickListener {
+            val intent = Intent(this, TimeRecommendationActivity::class.java)
+            startActivity(intent)
+        }
+        button_main_weather.setOnClickListener {
+
+        }
+        button_main_feeling.setOnClickListener {
+
+        }
+
     }
+
 }

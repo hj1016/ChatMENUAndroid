f75f0f3a71084c8e715580a3cc79bb40a5352834
catapillar0505 <twrp3301@gmail.com>
2024-01-29 12:01:40 +0900

    +버튼 눌렀을 때 버튼추가되는 ui (recyclerview) 자잘한 오류 수정

--- app/src/main/java/com/example/androidtest/MainOptionActivity.kt
+++ app/src/main/java/com/example/androidtest/MainOptionActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.myapplication.FeelingRecommendationActivity
 
 class MainOptionActivity : AppCompatActivity() {
     //ui 요소 선언

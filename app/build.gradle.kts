c3a78380ade87a7801011a5a79fe7e87f531a4bf
catapillar0505 <twrp3301@gmail.com>
2024-01-29 09:07:08 +0900

    ChatGPTConnection 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    //기본
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.5.0")
@@ -46,6 +46,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //추가
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
 
 
 

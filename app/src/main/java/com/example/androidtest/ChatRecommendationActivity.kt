085e8e0869028ed9dba5d2055a8eda067cde6b65
jiwon <knowledgeis1ne@gmail.com>
2024-02-04 13:05:57 +0900

    채팅으로 추천받기 - 유저와 챗지피티 메시지 구분하여 다르게 출력

--- app/src/main/java/com/example/androidtest/ChatRecommendationActivity.kt
+++ app/src/main/java/com/example/androidtest/ChatRecommendationActivity.kt
@@ -21,6 +21,8 @@ import kotlinx.coroutines.launch
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+var myMessage: Boolean = true
+
 // 자유롭게 말을 입력하여 대화하며 음식을 추천받을 수 있는 기능
 class ChatRecommendationActivity : AppCompatActivity() {
 
@@ -31,7 +33,8 @@ class ChatRecommendationActivity : AppCompatActivity() {
     lateinit var button_chat_send: Button // 전송 버튼
     lateinit var editText_chat_input: EditText // 채팅 입력 칸
 
-    var chat_list = arrayListOf<String>() // 내 채팅 내역을 담을 ArrayList
+
+    var chat_list = arrayListOf<Pair<Boolean, String>>() // 내 채팅 내역을 담을 ArrayList
     val chatListAdapter = ChatListAdapter(this, chat_list) // 어댑터 초기화
 
 
@@ -50,6 +53,7 @@ class ChatRecommendationActivity : AppCompatActivity() {
         // 전송 버튼 클릭 리스너
         button_chat_send.setOnClickListener {
             addBubbleRight()
+
             lifecycleScope.launch {
                 Toast.makeText(this@ChatRecommendationActivity, "대화 답변 생성에 10-20초 정도 소요됩니다", Toast.LENGTH_SHORT).show()
                 addBubbleLeft()
@@ -72,11 +76,13 @@ class ChatRecommendationActivity : AppCompatActivity() {
 
     // 내 메시지 말풍선 텍스트 뷰 동적 생성
     fun addBubbleRight() {
+        myMessage = true
+
         // 텍스트 뷰에 들어갈 String 생성
         val text = editText_chat_input.text.toString()
 
         // 채팅 리스트에 추가
-        chat_list.add(text)
+        chat_list.add(Pair(myMessage, text))
 
         // 리스트가 변동됨을 Adapter에 알림
         chatListAdapter.notifyDataSetChanged()
@@ -84,11 +90,13 @@ class ChatRecommendationActivity : AppCompatActivity() {
 
     // ChatGPT의 메시지 말풍선 텍스트 뷰 동적 생성
     suspend fun addBubbleLeft() {
+        myMessage = false
+
         // 텍스트 뷰에 들어갈 String 생성
         val text = chatGPTRequest(editText_chat_input.text.toString())
 
         // 채팅 리스트에 추가
-        chat_list.add(text)
+        chat_list.add(Pair(myMessage, text))
 
         // 리스트가 변동됨을 Adapter에 알림
         chatListAdapter.notifyDataSetChanged()
@@ -104,7 +112,7 @@ class ChatRecommendationActivity : AppCompatActivity() {
     }
 }
 
-class ChatListAdapter (val context: Context, val chat_list: ArrayList<String>): BaseAdapter() {
+class ChatListAdapter (val context: Context, val chat_list: ArrayList<Pair<Boolean, String>>): BaseAdapter() {
     override fun getCount(): Int {
         return chat_list.size
     }
@@ -118,16 +126,50 @@ class ChatListAdapter (val context: Context, val chat_list: ArrayList<String>): 
     }
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        // LayoutInflater: item을 Adapter에서 사용할 View로 inflate
-        val view: View = LayoutInflater.from(context).inflate(R.layout.textview_chat_right, null)
+        val isMyMessage = chat_list[p0].first
 
-        // 위에서 생성된 view를 textView_chat_left.xml 파일의 view와 연결
-        val textView_chat_left = view.findViewById<TextView>(R.id.textView_chat_right)
+        val layoutInflater = LayoutInflater.from(context)
 
-        // chat_list의 변수의 데이터를 TextView에 저장
-        val chat = chat_list[p0]
-        textView_chat_left.text = chat
+        if (isMyMessage) {
+            val view: View = layoutInflater.inflate(R.layout.textview_chat_right, null)
+            val textViewRight = view.findViewById<TextView>(R.id.textView_chat_right)
+            textViewRight.text = chat_list[p0].second
+            return view
+        } else {
+            val view: View = layoutInflater.inflate(R.layout.textview_chat_left, null)
+            val textViewLeft = view.findViewById<TextView>(R.id.textView_chat_left)
+            textViewLeft.text = chat_list[p0].second
+            return view
+        }
+/*
+        when(isMyMessage) {
+            true -> {
+                // LayoutInflater: item을 Adapter에서 사용할 View로 inflate
+                val view: View = LayoutInflater.from(context).inflate(R.layout.textview_chat_right, null)
 
-        return view
+                // 위에서 생성된 view를 textView_chat_left.xml 파일의 view와 연결
+                val textView_chat_right = view.findViewById<TextView>(R.id.textView_chat_right)
+
+                // chat_list의 변수의 데이터를 TextView에 저장
+                val chat = chat_list[p0]
+                textView_chat_right.text = chat
+
+                return view
+            }
+            false -> {
+                // LayoutInflater: item을 Adapter에서 사용할 View로 inflate
+                val view: View = LayoutInflater.from(context).inflate(R.layout.textview_chat_left, null)
+
+                // 위에서 생성된 view를 textView_chat_left.xml 파일의 view와 연결
+                val textView_chat_left = view.findViewById<TextView>(R.id.textView_chat_left)
+
+                // chat_list의 변수의 데이터를 TextView에 저장
+                val chat = chat_list[p0]
+                textView_chat_left.text = chat
+
+                return view
+            }
+        }
+ */
     }
 }
